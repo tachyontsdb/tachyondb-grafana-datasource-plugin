281f6da7ad2bfc9d0c8a8700f8a1b78deaae9556
Rajat Patwari <rajatpatwari@gmail.com>
2025-01-21 16:50:57 -0500

    Initial commit

---
+++ src/components/QueryEditor.tsx
@@ -0,0 +1,29 @@
+import { QueryEditorProps } from "@grafana/data";
+import { InlineField, Input, Stack } from "@grafana/ui";
+import React, { ChangeEvent } from "react";
+import { DataSource } from "../datasource";
+import { MyDataSourceOptions, MyQuery } from "../types";
+
+type Props = QueryEditorProps<DataSource, MyQuery, MyDataSourceOptions>;
+
+export function QueryEditor({ query, onChange, onRunQuery }: Props) {
+  const onQueryTextChange = (event: ChangeEvent<HTMLInputElement>) => {
+    onChange({ ...query, queryText: event.target.value });
+  };
+
+  const { queryText } = query;
+
+  return (
+    <Stack gap={0}>
+      <InlineField label="Query Text" labelWidth={16} tooltip="Not used yet">
+        <Input
+          id="query-editor-query-text"
+          onChange={onQueryTextChange}
+          value={queryText || ""}
+          required
+          placeholder="Enter a query"
+        />
+      </InlineField>
+    </Stack>
+  );
+}
---
+++ src/module.ts
@@ -0,0 +1,10 @@
+import { DataSourcePlugin } from "@grafana/data";
+
+import { ConfigEditor } from "./components/ConfigEditor";
+import { QueryEditor } from "./components/QueryEditor";
+import { DataSource } from "./datasource";
+import { MyDataSourceOptions, MyQuery } from "./types";
+
+export const plugin = new DataSourcePlugin<DataSource, MyQuery, MyDataSourceOptions>(DataSource)
+  .setConfigEditor(ConfigEditor)
+  .setQueryEditor(QueryEditor);
